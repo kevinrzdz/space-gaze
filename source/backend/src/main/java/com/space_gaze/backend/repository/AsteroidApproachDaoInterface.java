a3c4df6281fe43012754303c0b15809cf65be1a7
Kevin Ruiz <kevin.rf.es@gmail.com>
2023-04-07 17:38:04 +0100

    Añadida interfaz AsteroidApproachDaoInterface; Closes #19

---
+++ source/backend/src/main/java/com/space_gaze/backend/repository/AsteroidApproachDaoInterface.java
@@ -0,0 +1,7 @@
+package com.space_gaze.backend.repository;
+
+import com.space_gaze.backend.entity.AsteroidApproach;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AsteroidApproachDaoInterface extends JpaRepository<AsteroidApproach, Integer> {
+}
