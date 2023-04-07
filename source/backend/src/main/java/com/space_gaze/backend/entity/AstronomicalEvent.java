d79849ecc2a1b9834297d13e9f767debcca471d6
Kevin Ruiz <kevin.rf.es@gmail.com>
2023-04-07 13:22:52 +0100

    Entidades actualizadas

---
+++ source/backend/src/main/java/com/space_gaze/backend/entity/AstronomicalEvent.java
@@ -0,0 +1,91 @@
+package com.space_gaze.backend.entity;
+
+import jakarta.persistence.*;
+
+import java.sql.Timestamp;
+import java.util.Collection;
+import java.util.Objects;
+
+@Entity
+@Table(name = "astronomical_event")
+public class AstronomicalEvent {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id", nullable = false)
+    private int id;
+    @Basic
+    @Column(name = "name", nullable = false, length = 255)
+    private String name;
+    @Basic
+    @Column(name = "description", nullable = true, length = -1)
+    private String description;
+    @Basic
+    @Column(name = "start_date", nullable = false)
+    private Timestamp startDate;
+    @Basic
+    @Column(name = "end_date", nullable = true)
+    private Timestamp endDate;
+    @OneToMany(mappedBy = "astronomicalEventByObjectId")
+    private Collection<UserTracking> userTrackingsById;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Timestamp getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Timestamp startDate) {
+        this.startDate = startDate;
+    }
+
+    public Timestamp getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Timestamp endDate) {
+        this.endDate = endDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AstronomicalEvent that = (AstronomicalEvent) o;
+        return id == that.id && Objects.equals(name, that.name) && Objects.equals(description, that.description) && Objects.equals(startDate, that.startDate) && Objects.equals(endDate, that.endDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, startDate, endDate);
+    }
+
+    public Collection<UserTracking> getUserTrackingsById() {
+        return userTrackingsById;
+    }
+
+    public void setUserTrackingsById(Collection<UserTracking> userTrackingsById) {
+        this.userTrackingsById = userTrackingsById;
+    }
+}
---
+++ source/backend/src/main/java/com/space_gaze/backend/entity/Star.java
@@ -0,0 +1,90 @@
+package com.space_gaze.backend.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Collection;
+import java.util.Objects;
+
+@Entity
+@Table(name = "star")
+public class Star {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id", nullable = false)
+    private int id;
+    @Basic
+    @Column(name = "name", nullable = false, length = 255)
+    private String name;
+    @Basic
+    @Column(name = "constellation", nullable = false, length = 255)
+    private String constellation;
+    @Basic
+    @Column(name = "magnitude", nullable = false, length = 100)
+    private String magnitude;
+    @Basic
+    @Column(name = "distance_earth", nullable = false, length = 100)
+    private String distanceEarth;
+    @OneToMany(mappedBy = "starByObjectId")
+    private Collection<UserTracking> userTrackingsById;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getConstellation() {
+        return constellation;
+    }
+
+    public void setConstellation(String constellation) {
+        this.constellation = constellation;
+    }
+
+    public String getMagnitude() {
+        return magnitude;
+    }
+
+    public void setMagnitude(String magnitude) {
+        this.magnitude = magnitude;
+    }
+
+    public String getDistanceEarth() {
+        return distanceEarth;
+    }
+
+    public void setDistanceEarth(String distanceEarth) {
+        this.distanceEarth = distanceEarth;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Star star = (Star) o;
+        return id == star.id && Objects.equals(name, star.name) && Objects.equals(constellation, star.constellation) && Objects.equals(magnitude, star.magnitude) && Objects.equals(distanceEarth, star.distanceEarth);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, constellation, magnitude, distanceEarth);
+    }
+
+    public Collection<UserTracking> getUserTrackingsById() {
+        return userTrackingsById;
+    }
+
+    public void setUserTrackingsById(Collection<UserTracking> userTrackingsById) {
+        this.userTrackingsById = userTrackingsById;
+    }
+}
