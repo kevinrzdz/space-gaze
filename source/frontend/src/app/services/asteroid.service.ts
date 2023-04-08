fba2d80b4c2665291f540636d1bb23739f075f16
Kevin Ruiz <kevin.rf.es@gmail.com>
2023-04-08 15:59:54 +0100

    Añadido servicio para asteroides en Angular; Closes #24

---
+++ source/frontend/src/app/services/asteroid.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Asteroid } from '../models/asteroid.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AsteroidService {
+  private apiUrl = 'http://localhost:8080/api';
+
+  constructor(private http: HttpClient) {}
+
+  getAsteroids(
+    page: number
+  ): Observable<{ content: Asteroid[]; totalElements: number }> {
+    return this.http.get<{ content: Asteroid[]; totalElements: number }>(
+      `${this.apiUrl}/asteroids?page=${page}`
+    );
+  }
+
+}
