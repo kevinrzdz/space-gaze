0bc78ece5cebd38b06786e19eed32f01cf4f87a4
Kevin Ruiz <kevin.rf.es@gmail.com>
2023-04-15 20:03:37 +0100

    Navbar actualizado

--- source/frontend/src/app/app.module.ts
+++ source/frontend/src/app/app.module.ts
@@ -5,11 +5,11 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import {AsteroidComponent} from './components/asteroid/asteroid.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {LoginComponent} from './components/login/login.component';
+import {RegisterComponent} from './components/register/register.component';
 import {NgOptimizedImage} from "@angular/common";
 
 @NgModule({
@@ -18,17 +18,17 @@ import {NgOptimizedImage} from "@angular/common";
     AsteroidComponent,
     NavbarComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        HttpClientModule,
-        NgbModule,
-        FormsModule,
-        NgOptimizedImage,
-        ReactiveFormsModule,
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule,
+    FormsModule,
+    NgOptimizedImage,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
