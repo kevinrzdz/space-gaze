e8c048d5f1268c3b628ac9aa29877fc5fe961a01
Kevin Ruiz <kevin.rf.es@gmail.com>
2023-04-23 22:23:27 +0100

    Revert "Implementado CRUD de eventos astronómicos Closes #34"
    This reverts commit 20ff4ff672ce53b88474ee13f566b8d55f477b3b.

--- source/backend/src/main/java/com/space_gaze/backend/repository/AstronomicalEventRepository.java
+++
@@ -1,7 +0,0 @@
-package com.space_gaze.backend.repository;
-
-import com.space_gaze.backend.entity.AstronomicalEvent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AstronomicalEventRepository extends JpaRepository<AstronomicalEvent, Integer> {
-}
