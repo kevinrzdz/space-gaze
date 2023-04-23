d42044a357f0c1f20c475ae86c9a200d858d85c1
Kevin Ruiz <kevin.rf.es@gmail.com>
2023-04-23 22:21:38 +0100

    Añadida página planetas; Closes #35

---
+++ source/frontend/src/app/services/planet/planet.service.ts
@@ -0,0 +1,23 @@
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {Planet} from "../../models/planet/planet.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlanetService {
+
+  apiUrl: string = 'http://localhost:8090/api/planets';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getPlanets(): Observable<Planet[]> {
+    return this.http.get<Planet[]>(this.apiUrl);
+  }
+
+  getPlanet(id: number): Observable<Planet> {
+    return this.http.get<Planet>(`${this.apiUrl}/${id}`);
+  }
+}
