662b14b91fb8246eb62ef2ce484c4117c9120d2a
Kevin Ruiz <kevin.rf.es@gmail.com>
2023-04-23 22:10:43 +0100

    Creada interfaz genérica para servicios; Closes #30

---
+++ source/backend/src/main/java/com/space_gaze/backend/service/CrudServiceInterface.java
@@ -0,0 +1,20 @@
+package com.space_gaze.backend.service;
+
+import com.space_gaze.backend.entity.Asteroid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface CrudServiceInterface<T, E> {
+
+    List<T> findAll();
+
+    Page<T> findAll(Pageable pageable);
+
+    T findById(E id);
+
+    T save(T entity);
+
+    void deleteById(E id);
+}
