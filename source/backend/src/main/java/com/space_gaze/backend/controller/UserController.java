4b50b611941479991c5086c6d86e9ce0f8fd9f92
Kevin Ruiz <kevin.rf.es@gmail.com>
2023-05-03 21:29:23 +0100

    Implementada subida de imágenes a asteroides, backend; Closes #40

--- source/backend/src/main/java/com/space_gaze/backend/controller/UserController.java
+++ source/backend/src/main/java/com/space_gaze/backend/controller/UserController.java
@@ -7,6 +7,7 @@ import com.space_gaze.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
