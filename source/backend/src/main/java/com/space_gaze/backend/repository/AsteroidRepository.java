7d457477ca6e9dfe77c46c23c200e72d6522492c
Kevin Ruiz <kevin.rf.es@gmail.com>
2023-05-07 22:55:00 +0100

    Eliminados métodos innecesarios en repositorio; Closes #46

--- source/backend/src/main/java/com/space_gaze/backend/repository/AsteroidRepository.java
+++ source/backend/src/main/java/com/space_gaze/backend/repository/AsteroidRepository.java
@@ -1,13 +1,8 @@
 package com.space_gaze.backend.repository;
 
 import com.space_gaze.backend.entity.Asteroid;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface AsteroidRepository extends JpaRepository<Asteroid, Integer> {
-    @Query("SELECT a FROM Asteroid a WHERE LOWER(a.name) LIKE LOWER(CONCAT('%', :name, '%'))")
-    Page<Asteroid> findByName(String name, Pageable pageable);
 }
 
--- source/backend/src/main/java/com/space_gaze/backend/repository/ExoplanetRepository.java
+++ source/backend/src/main/java/com/space_gaze/backend/repository/ExoplanetRepository.java
@@ -1,12 +1,7 @@
 package com.space_gaze.backend.repository;
 
 import com.space_gaze.backend.entity.Exoplanet;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface ExoplanetRepository extends JpaRepository<Exoplanet, Integer> {
-    @Query("SELECT e FROM Exoplanet e WHERE LOWER(e.name) LIKE LOWER(CONCAT('%', :name, '%'))")
-    Page<Exoplanet> findByName(String name, Pageable pageable);
 }
