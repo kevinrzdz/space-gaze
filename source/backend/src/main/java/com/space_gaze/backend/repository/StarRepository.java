de39845fcd9394819b01404dcb8f047d1e91e3ae
Kevin Ruiz <kevin.rf.es@gmail.com>
2023-05-07 22:55:00 +0100

    Eliminados métodos innecesarios en repositorio; Closes #46

--- source/backend/src/main/java/com/space_gaze/backend/repository/StarRepository.java
+++ source/backend/src/main/java/com/space_gaze/backend/repository/StarRepository.java
@@ -1,12 +1,7 @@
 package com.space_gaze.backend.repository;
 
 import com.space_gaze.backend.entity.Star;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface StarRepository extends JpaRepository<Star, Integer> {
-    @Query("SELECT s FROM Star s WHERE LOWER(s.name) LIKE LOWER(CONCAT('%', :name, '%'))")
-    Page<Star> findByName(String name, Pageable pageable);
 }
