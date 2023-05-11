22c4388a4a078fd21eea2805681203de67283f8a
Kevin Ruiz <kevin.rf.es@gmail.com>
2023-05-11 08:56:03 +0100

    Finalizado registro de usuarios

--- source/frontend/src/app/components/login/login.component.ts
+++ source/frontend/src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {AuthService} from '../../services/auth/auth.service';
 import {Router} from '@angular/router';
+import {Login} from "../../models/user/login.model";
 
 @Component({
   selector: 'app-login',
@@ -8,25 +9,24 @@ import {Router} from '@angular/router';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-
+  loginData: Login = {
+    email: '',
+    password: ''
+  };
   errorMessage!: string;
 
   constructor(private authService: AuthService, private router: Router) {
   }
 
-  // login() {
-  //  this.authService.login(this.loginData).subscribe(response => {
-  //    if (response.status) {
-  //      this.router.navigate(['/home']);
-  //    } else {
-  //      this.errorMessage = response.message;
-  //    }
-  //  });
-  // }
+  login() {
+    this.authService.login(this.loginData).subscribe(response => {
+      console.log(response);
+    });
+  }
 
-  // onEnter(event: any) {
-  //   if (event.keyCode === 13) {
-  //    this.login();
-  //  }
-  // }
+  onEnter(event: any) {
+    if (event.keyCode === 13) {
+      this.login();
+    }
+  }
 }
