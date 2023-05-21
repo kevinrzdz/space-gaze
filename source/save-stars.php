f69c68cfed92a6af71f643b44e9312a2499fdee3
Kevin Ruiz <kevin.rf.es@gmail.com>
2023-05-21 22:02:44 +0100

    Actualizados scripts de interacción con apis

--- source/save-stars.php
+++ source/save-stars.php
@@ -1,31 +1,16 @@
 <?php
-// Establecer las variables de conexión a la base de datos
-$host = "localhost";
-$dbname = "asteroids";
-$username = "asteroids_app";
-$password = "asteroids_app2023";
 
-// Establecer la conexión a la base de datos usando PDO
-try {
-  $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-} catch (PDOException $e) {
-  die("Error al conectarse a la base de datos: " . $e->getMessage());
-}
+require_once "database.php";
 
-// Establecer las opciones para PDO
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-// Establecer la URL de la API y el API Key
 $url = "https://api.api-ninjas.com/v1/stars";
-$api_key = "gINtdhWYEx2CD2VCTle9ug==VY575ynAYuYezmdC";
 
-// Establecer los parámetros para la solicitud GET
 $params = array(
   "max_distance_light_year" => 254000,
   "offset" => 0
 );
 
-// Establecer los encabezados para la solicitud GET
 $headers = array(
   "X-Api-Key: $api_key"
 );
@@ -37,7 +22,6 @@ do {
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   $response = curl_exec($ch);
   curl_close($ch);
-  // Decodificar la respuesta JSON en un array asociativo
   $data = json_decode($response, true);
 
   foreach ($data as $star) {
