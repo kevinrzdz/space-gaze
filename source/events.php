d655870115d022ba6fb50f531284a02344521d6a
Kevin Ruiz <kevin.rf.es@gmail.com>
2023-05-21 22:02:44 +0100

    Actualizados scripts de interacción con apis

--- source/events.php
+++ source/events.php
@@ -1,28 +1,19 @@
 <?php
-// Definir la URL de la API
+
+require_once "database.php";
 $url = "https://astronomyapi.com/api/ephemeris";
 
-// Inicializar cURL
 $ch = curl_init();
 
-// Establecer las opciones de cURL
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-// Ejecutar cURL y obtener la respuesta
 $response = curl_exec($ch);
 
-// Cerrar cURL
 curl_close($ch);
 
-// Verificar si la respuesta es válida
 if ($response !== false) {
-  // Convertir la respuesta JSON a un objeto PHP
   $data = json_decode($response);
-  // Mostrar algunos datos de la API
-  echo "El sol sale a las " . $data->sunrise . "<br/>";
-  echo "La luna está en fase de " . $data->moon_phase . "<br/>";
 } else {
-  // Mostrar un mensaje de error
   echo "No se pudo obtener los datos de la API";
 }
