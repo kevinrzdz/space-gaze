fc5a22453aa122d6a3cbf5bb8bbc9fd04911cbc6
Kevin Ruiz <kevin.rf.es@gmail.com>
2023-05-21 14:13:42 +0100

    JWT actualizado

--- source/backend/src/main/java/com/space_gaze/backend/security/TokenUtils.java
+++ source/backend/src/main/java/com/space_gaze/backend/security/TokenUtils.java
@@ -17,13 +17,14 @@ public class TokenUtils {
 	private final static String ACCESS_TOKEN_SECRET = "74dec2d4102642138e5dc78d97f8a96c";
 	private final static Long ACCESS_TOKEN_VALIDITY_SECONDS = 2_592_000L;
 	
-	public static String createToken(String nombre, String email) {
+	public static String createToken(String name, String email, Integer id) {
 		long expirationTime = ACCESS_TOKEN_VALIDITY_SECONDS * 1000;
 		Date expirationDate = new Date(System.currentTimeMillis() + expirationTime);
 		
 		Map<String, Object> extra = new HashMap<>();
-		extra.put("nombre", nombre);
-		
+		extra.put("name", name);
+		extra.put("id", id);
+
 		return Jwts.builder()
 				.setSubject(email)
 				.setExpiration(expirationDate)
